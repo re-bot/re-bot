7736428451f7ffde2546380ded2206f1892a9cd9
Vincent Talbot <vincent.talbot@gmail.com>
2014-07-11 01:16:28 -0400

    Initial commit.

---
+++ src/ReBot/ReBot/ReBotServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace ReBot\ReBot;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReBotServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
